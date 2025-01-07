9d5206258c0cb932749f0902aacaba5de3cd75fa
jgomper <jgomper@upv.edu.es>
2025-01-07 15:40:52 +0100

    Añadida declaración de la clase Point2D

--- Point2D.h
+++ Point2D.h
@@ -6,32 +6,19 @@
 
 class Point2D {
 public:
-    double x;
-    double y;
+    double x; // Coordenada x
+    double y; // Coordenada y
 
     // Constructor
-    Point2D(double x = 0, double y = 0) : x(x), y(y) {}
+    Point2D(double x = 0, double y = 0);
 
-    // Calcula la distancia euclidiana entre dos puntos
-    static double distance(const Point2D &a, const Point2D &b) {
-        return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
-    }
+    // Métodos estáticos
+    static double distance(const Point2D &a, const Point2D &b);
 
-    // Sobrecarga del operador ==
-    friend bool operator==(const Point2D &a, const Point2D &b) {
-        return a.x == b.x && a.y == b.y;
-    }
-
-    // Sobrecarga del operador !=
-    friend bool operator!=(const Point2D &a, const Point2D &b) {
-        return !(a == b);
-    }
-
-    // Sobrecarga del operador << para imprimir el punto
-    friend std::ostream &operator<<(std::ostream &out, const Point2D &p) {
-        out << "(" << p.x << ", " << p.y << ")";
-        return out;
-    }
+    // Sobrecarga de operadores
+    friend bool operator==(const Point2D &a, const Point2D &b);
+    friend bool operator!=(const Point2D &a, const Point2D &b);
+    friend std::ostream &operator<<(std::ostream &out, const Point2D &p);
 };
 
 #endif
