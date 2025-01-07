4545a08dba6be6235cbe9765a5eeb26d1ed50086
jgomper <jgomper@upv.edu.es>
2025-01-07 11:43:34 +0100

    Añadida implementación de la clase Node

--- Node.h
+++ Node.h
@@ -1,45 +1,18 @@
 #include <ostream>
 
-template <typename T> 
-class Node {
-    public:
-        // Miembros de datos
-        T data;  // Valor del nodo
-        Node* next;  // Puntero al siguiente nodo
-
-        // Constructor (modificado para aceptar valor y puntero al siguiente nodo)
-        Node(T val, Node* nextNode = nullptr) : data(val), next(nextNode) {}
-
-        // Destructor
-        ~Node() {
-            // Aquí podrías liberar recursos si fuera necesario
-        }
-
-        // Método para establecer el siguiente nodo
-        void setNext(Node* nextNode) {
-            next = nextNode;
-        }
-
-        // Método para obtener el siguiente nodo
-        Node* getNext() const {
-            return next;
-        }
-
-        // Método para obtener el valor del nodo
-        T getValue() const {
-            return data;
-        }
-
-        // Método para imprimir el nodo (opcional)
-        void print(std::ostream& out) const {
-            out << data;
-        }
-};
-
-// Sobrecarga del operador de salida para imprimir el nodo
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
-    node.print(out);
-    return out;
-}
+class Node {
+public:
+    T data;                // Elemento almacenado en el nodo
+    Node<T>* next;         // Puntero al siguiente nodo (nullptr si es el último)
+
+    // Constructor
+    Node(T data, Node<T>* next = nullptr) : data(data), next(next) {}
+
+    // Sobrecarga del operador << para imprimir el nodo
+    friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+        out << node.data;
+        return out;
+    }
+};
 
